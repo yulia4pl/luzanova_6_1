2737903c022c6083d35cc9ec40c2b1355b980d96
yulia4pl <yulia4pl@gmail.com>
2024-02-21 11:58:44 +0100

    MUL command

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 #python3 app.py add 9 5
 #python3 app.py sub 9 5
+#python3 app.py mul 9 5
 
 import sys
 
@@ -8,15 +9,17 @@ if __name__ == "__main__":
     try:
         if len(sys.argv) != 4:
             print(sys.argv)
-            raise NameError("Enter 4 paramenrs: program name, command add/sub, number1, number2")
+            raise NameError("Enter 4 paramenrs: program name, command add/sub/mul, number1, number2")
         print(sys.argv)
        
         if (sys.argv[1] == "add"):
             print(int(sys.argv[2]) + int(sys.argv[3]))
         elif sys.argv[1] == "sub":
-            print(int(sys.argv[2]) - int(sys.argv[3]))          
+            print(int(sys.argv[2]) - int(sys.argv[3]))
+        elif sys.argv[1] == "mul":
+            print(int(sys.argv[2]) * int(sys.argv[3]))           
         else:
-            print("Unknown command. It can be add/sub.")                
+            print("Unknown command. It can be add/sub/mul.")                
     
     except Exception as err:
         print(err)
