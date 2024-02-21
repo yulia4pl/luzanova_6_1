76d3ded4edb91e78490ae1a93d318bc970d2d6d0
yulia4pl <yulia4pl@gmail.com>
2024-02-21 11:56:12 +0100

    First commit: ADD and SUB

---
+++ app.py
@@ -0,0 +1,22 @@
+#python3 app.py add 9 5
+#python3 app.py sub 9 5
+
+import sys
+
+if __name__ == "__main__":
+
+    try:
+        if len(sys.argv) != 4:
+            print(sys.argv)
+            raise NameError("Enter 4 paramenrs: program name, command add/sub, number1, number2")
+        print(sys.argv)
+       
+        if (sys.argv[1] == "add"):
+            print(int(sys.argv[2]) + int(sys.argv[3]))
+        elif sys.argv[1] == "sub":
+            print(int(sys.argv[2]) - int(sys.argv[3]))          
+        else:
+            print("Unknown command. It can be add/sub.")                
+    
+    except Exception as err:
+        print(err)
